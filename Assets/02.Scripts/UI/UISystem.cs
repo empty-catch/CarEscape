34331de7af7d3153c1aa99139331fb47cec8b8c7
Komorio <twilivez@gmail.com>
2020-06-06 00:18:42 +0900

    [UPDATE] 폴더 이름 변경

---
+++ Assets/02.Scripts/UI/UISystem.cs
@@ -0,0 +1,66 @@
+#pragma warning disable CS0649
+
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class UISystem : MonoBehaviour
+{
+    [SerializeField]
+    private Text heartCount;
+    [SerializeField]
+    private Timer timer;
+    [SerializeField]
+    private GameObject continueStage;
+    [SerializeField]
+    private Text continueElapsed;
+    [SerializeField]
+    private GameObject finalStageCleared;
+    [SerializeField]
+    private Text finalStageElapsed;
+
+    public void StageCleared()
+    {
+        timer.IsPaused = true;
+        DOVirtual.DelayedCall(0.4F, () =>
+        {
+            if (Stage.AllCleared)
+            {
+                SceneManager.LoadScene("03.EndingScene");
+            }
+            else if (Stage.Current == 3)
+            {
+                finalStageElapsed.text = timer.Elapsed;
+                finalStageCleared.SetActive(true);
+            }
+            else
+            {
+                continueElapsed.text = timer.Elapsed;
+                continueStage.SetActive(true);
+            }
+        });
+    }
+
+    public void LoadNextStage()
+    {
+        if (Stage.Current == 3)
+        {
+            throw new Exception("There is no more stage exists.");
+        }
+
+        Stage.Current++;
+        LoadScene("02.InGameScene");
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void UpdateHeart()
+    {
+        heartCount.text = Heart.Count.ToString();
+    }
+}
